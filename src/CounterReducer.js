9d005e08a0197d34e1d406b6a608132d7dba626b
Sikandar khan <sikandar_khan9370@yahoo.com>
2020-06-20 16:12:16 +0500

    Added default and removed import React

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -2,6 +2,8 @@ const counterReducer = (state, action) => {
     switch (action) {
       case 'INCREMENT':
         return state + 1
+      default:
+          throw new Error("Not Wroking");
     }
   }
 
